6eaf4786c28e124ee907c55a934327c93ae2658d
philip31si <philipsizeon@gmail.com>
2025-01-27 14:49:05 +0530

    git commit -m "new changes updated"

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -4,9 +4,11 @@ import org.joda.time.LocalTime;
 
 public class HelloWorld {
     public static void main(String[] args) {
-      LocalTime currentTime = new LocalTime();
-		  System.out.println("The current local time is: " + currentTime);
+        // Print the current local time using Joda-Time
+        LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is: " + currentTime);
 
+        // Create an instance of the Greeter class and print the greeting
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
     }
